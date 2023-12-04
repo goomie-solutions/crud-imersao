43cc0967ed4c6184a629b5b09834fda6bce5ac26
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-12-04 15:53:25 -0300

    implementando as exceções no serviço

--- src/main/java/com/backend/servicos/UsuarioServico.java
+++ src/main/java/com/backend/servicos/UsuarioServico.java
@@ -1,10 +1,14 @@
 package com.backend.servicos;
 
 import com.backend.entidades.Usuario;
+import com.backend.infra.handler.excecoes.CampoObrigatorioException;
+import com.backend.infra.handler.excecoes.UsuarioNaoEncontradoException;
 import com.backend.repositorios.UsuarioRepositorio;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UsuarioServico {
@@ -14,17 +18,28 @@ public class UsuarioServico {
 
     public Usuario consultaUsuario(Long id) {
         Usuario usuario = repositorio.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Usuario nao encontrado"));
+                .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario nao encontrado"));
 
         return usuario;
     }
 
     public Usuario criaUsuario(Usuario user) {
+        List<String> campo = checaCampos(user);
+
+        if(!campo.isEmpty()) {
+            String mensagem = "O(s) seguinte(s) campo(s) é(são) obrigatório(s):" +
+                    String.join(", ", campo);
+            throw new CampoObrigatorioException(mensagem);
+        }
+
         Usuario usuario = repositorio.save(user);
         return usuario;
     }
 
     public void deletaUsuario(Long id) {
+        Usuario usuario = repositorio.findById(id)
+                .orElseThrow(() -> new UsuarioNaoEncontradoException(
+                        String.format("Usuário de id %01d não foi encontrado", id)));
         repositorio.deleteById(id);
     }
 
@@ -40,5 +55,15 @@ public class UsuarioServico {
         usuarioQueVemDoBancoDeDados.setNome(requisicao.getNome());
         usuarioQueVemDoBancoDeDados.setEmail(requisicao.getEmail());
         usuarioQueVemDoBancoDeDados.setSenha(requisicao.getSenha());
+    }
+
+    private List checaCampos(Usuario usuario) {
+        List<String> resultado = new ArrayList<>();
+
+        if(usuario.getNome() == null) resultado.add("nome");
+        if(usuario.getEmail() == null) resultado.add("e-mail");
+        if(usuario.getSenha() == null) resultado.add("senha");
+
+        return resultado;
     }
 }
